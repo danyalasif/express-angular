13e8137628424ea9b63ea87efd8a60565b7b9c00
Danyal <7072946+danyalasif@users.noreply.github.com>
2023-05-13 13:49:32 +0500

    Squashed commit of the following:
    commit 174f282230ae602947213afc3149ba0eea710670
    Author: Danyal <7072946+danyalasif@users.noreply.github.com>
    Date:   Sat May 13 13:46:14 2023 +0500
    
        remove render.yaml
    
    commit 16007f11c961271f322bc50d539914dcf9ea5bcb
    Author: Danyal <7072946+danyalasif@users.noreply.github.com>
    Date:   Sat May 13 00:04:38 2023 +0500
    
        Add health check route
    
    commit 4d3450a2e4938ec05c15dac56ac8c89f3fa9cab8
    Author: Danyal <7072946+danyalasif@users.noreply.github.com>
    Date:   Fri May 12 17:44:33 2023 +0500
    
        Tweak buildCommand

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,8 +12,8 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use(express.json());
 
 app.use('/api/products', require('./routes/products'));
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.get('/api/health', (req, res) => {
+  res.status(200).send('OK');
 });
 
 const port = process.env.PORT || 3333;
